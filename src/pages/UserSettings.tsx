12d9a87d17a759c0a1eebe0cbb6b817551a9e2a1
Gian Carlo Torres <geanu@outlook.com>
2023-06-21 12:24:44 -0400

    UserSettings major updates: Save Responses.

--- src/pages/UserSettings.tsx
+++ src/pages/UserSettings.tsx
@@ -4,6 +4,7 @@ import { BrekkyContext } from "../contexts/BrekkyProvider"
 import { useContext, useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import HeaderTitle from "../components/HeaderTitle"
+import Spinner from "../components/Spinner"
 
 const base_api_url = import.meta.env.VITE_APP_BASE_API
 
@@ -14,14 +15,21 @@ interface Accountable {
     last_name?: string
 }
 
+interface ISaveResponse {
+    active: boolean,
+    success: boolean,
+    message: string
+}
+
 export default function UserSettings() {
 
     const { user, setUser } = useContext(BrekkyContext)
     const [ account, setAccount ] = useState<Accountable>()
     const [ updatedNames, setUpdatedNames ] = useState<Accountable>()
+    const [ saveResponse, setSaveResponse ] = useState<ISaveResponse>({active: false, success: false, message: ""})
     const navigate = useNavigate()
 
-    const { register, handleSubmit } = useForm()
+    const { register, reset, handleSubmit } = useForm()
 
     useEffect(() => {
         (async () => {
@@ -33,12 +41,17 @@ export default function UserSettings() {
                 }
             })
             if (res.ok) {
-                const data: Accountable = await res.json()
-                console.log(data)
-                setAccount(data)
+                const data = await res.json()
+                const arrData: Accountable = data[0]
+                console.log(arrData)
+                setAccount(arrData)
             }
         })()
     }, [])
+    
+    useEffect(() => {
+        reset(account)
+    }, [account])
 
     async function handleSaveAccount(data: any): Promise<SubmitHandler<Accountable>> {
         console.log(`react-form-hook-data: ${data}`)
@@ -52,14 +65,33 @@ export default function UserSettings() {
         })
         if (res.ok) {
             const dataRes = await res.json() 
-            console.log(dataRes)
-            // setUpdatedNames({ 
-            //     first_name: dataRes['first_name'], 
-            //     last_name: dataRes['last_name'],
-            //     email: dataRes['email'],
-            //     username: dataRes['username']
-            // })
-            // setUser(prevState => ({ ...prevState, firstname: dataRes['first_name'] }))
+            const dr = dataRes[0]
+            console.log(dr)
+            if (dr.success) {
+                setUpdatedNames({ 
+                    first_name: dataRes['first_name'], 
+                    last_name: dataRes['last_name'],
+                    email: dataRes['email'],
+                    username: dataRes['username']
+                })
+                setUser(prevState => ({ 
+                    ...prevState, 
+                    username: dr.username,
+                    firstname: dr['first_name'] 
+                }))
+                setSaveResponse({
+                    active: true,
+                    success: dr.success,
+                    message: dr.message})
+            } else {
+                setSaveResponse({
+                    active: true,
+                    success: dr.success,
+                    message: dr.message
+                })
+            }
+
+            
         }
         return data
     }
@@ -95,35 +127,44 @@ export default function UserSettings() {
                 <div>
                     <HeaderTitle unColoredText="Account " coloredText="Settings" />
                     <form onSubmit={handleSubmit(handleSaveAccount)} className="text-center">
-                        <input {...register('email')} defaultValue={account?.email} type="email" placeholder="Email Address" 
-                        className="mb-3 py-2 px-3 rounded-md text-lg text-gray-900 bg-gray-50 border border-gray-300 focus:ring-sky-500 focus:border-sky-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-sky-500 dark:focus:border-sky-500" disabled />
-                        <br />
+                        { account ? ( <>
+                            <input {...register('email')} defaultValue={account.email} type="email" placeholder="Email Address" 
+                            className="mb-3 py-2 px-3 rounded-md text-lg text-gray-900 bg-gray-50 border border-gray-300 focus:ring-sky-500 focus:border-sky-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-sky-500 dark:focus:border-sky-500" />
+                            <br />
+                        
+                            <input {...register('first_name')} type="text" placeholder="First Name" defaultValue={account.first_name}
+                            className="mb-3 py-2 px-3 rounded-md text-lg text-gray-900 bg-gray-50 border border-gray-300 focus:ring-sky-500 focus:border-sky-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-sky-500 dark:focus:border-sky-500"/>
+                            <br />
                     
-                        <input {...register('first_name')} type="text" placeholder="First Name" defaultValue={account?.first_name}
-                        className="mb-3 py-2 px-3 rounded-md text-lg text-gray-900 bg-gray-50 border border-gray-300 focus:ring-sky-500 focus:border-sky-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-sky-500 dark:focus:border-sky-500"/>
-                        <br />
+                            <input {...register('last_name')} type="text" placeholder="Last Name" defaultValue={account.last_name}
+                            className="mb-3 py-2 px-3 rounded-md text-lg text-gray-900 bg-gray-50 border border-gray-300 focus:ring-sky-500 focus:border-sky-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-sky-500 dark:focus:border-sky-500"/>
+                            <br />
                 
-                        <input {...register('last_name')} type="text" placeholder="Last Name" defaultValue={account?.last_name}
-                        className="mb-3 py-2 px-3 rounded-md text-lg text-gray-900 bg-gray-50 border border-gray-300 focus:ring-sky-500 focus:border-sky-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-sky-500 dark:focus:border-sky-500"/>
-                        <br />
-            
-                        <input {...register('username')} type="text" placeholder="Username" defaultValue={user.username}
-                        className="mb-3 py-2 px-3 rounded-md text-lg text-gray-900 bg-gray-50 border border-gray-300 focus:ring-sky-500 focus:border-sky-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-sky-500 dark:focus:border-sky-500" disabled/>
-                        <br />
-            
-                        <input {...register('password')} type="password" placeholder="Password" disabled
-                        className="mb-3 py-2 px-3 rounded-md text-lg text-gray-900 bg-gray-50 border border-gray-300 focus:ring-sky-500 focus:border-sky-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-sky-500 dark:focus:border-sky-500"/>
-                        <br />
-
+                            <input {...register('username')} type="text" placeholder="Username" defaultValue={account.username}
+                            className="mb-3 py-2 px-3 rounded-md text-lg text-gray-900 bg-gray-50 border border-gray-300 focus:ring-sky-500 focus:border-sky-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-sky-500 dark:focus:border-sky-500"/>
+                            <br />
+                
+                            <input {...register('password')} type="password" placeholder="Password" disabled
+                            className="mb-3 py-2 px-3 rounded-md text-lg text-gray-900 bg-gray-50 border border-gray-300 focus:ring-sky-500 focus:border-sky-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-sky-500 dark:focus:border-sky-500"/>
+                            <br />
+                            </>
+                            ) : (
+                                <Spinner />
+                            )
+                        }
                         <div className="flex justify-center mb-3"><CuteButton buttonDisplayName="Save Changes" /></div>
                         <div className="flex justify-center"><CuteButton eventHandler={handleCancel} buttonDisplayName="Cancel" /></div>
-                        <p className="mt-4 text-sm text-white italic">For now, users are only allowed to change their first and last names.</p>
+                        <p className="mt-4 text-sm text-white italic">...</p>
                     </form>
                     {
-                        updatedNames ? 
-                        <div className="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3 text-center mt-6" role="alert">
-                            <p className="font-bold">Success</p>
-                            <p className="text-sm">Account Info changed.</p>
+                        saveResponse.active ? 
+                        <div className={saveResponse.success ?
+                             `bg-blue-100 border-blue-500 text-blue-700` :
+                             `bg-red-100 border-red-500 text-red-700`
+                                + "border-t border-b px-4 py-3 text-center mt-6"
+                            } role="alert">
+                            <p className="font-bold">{saveResponse.success ? "Success" : "Error"}</p>
+                            <p className="text-sm">{saveResponse.message}</p>
                         </div> :
                         ''
                     }
