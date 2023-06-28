3ff88c2a7e49f10eee4ecbf62808c89ed218b7b9
Gian Carlo Torres <geanu@outlook.com>
2023-06-27 23:07:59 -0400

    Registration Header change

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,4 +1,5 @@
 import CuteButton from "../components/CuteButton"
+import HeaderTitle from "../components/HeaderTitle"
 import { BrekkyContext } from "../contexts/BrekkyProvider"
 import { useRef, useContext, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
@@ -74,13 +75,7 @@ export default function Register() {
         <>
             <div className="grid place-items-center h-screen">
                 <div>
-                <h2 className="mb-8 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
-                    Welcome to&nbsp;
-                    <span className="text-transparent bg-clip-text bg-gradient-to-r to-sky-200 from-sky-400">
-                    Brekky
-                    </span>
-                    !
-                </h2>
+                <HeaderTitle unColoredText="Welcome to " coloredText="Brekky!"/>
                 <form onSubmit={handleRegisterForm} className="text-center">
                     <input type="email" placeholder="Email Address" ref={emailField}
                     className="mb-3 py-2 px-3 rounded-md text-lg text-gray-900 bg-gray-50 border border-gray-300 focus:ring-sky-500 focus:border-sky-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-sky-500 dark:focus:border-sky-500" />
