3c1edae9e9f28941902c63f76a5eab8ba7ed8f66
Gian Carlo Torres <geanu@outlook.com>
2023-06-29 11:55:53 -0400

    Changes on UserRecipeEdit and Personalize

--- src/pages/UserRecipeEdit.tsx
+++ src/pages/UserRecipeEdit.tsx
@@ -116,7 +116,7 @@ export default function UserRecipeEdit() {
                             {...register('recipeName')}
                             type="text"
                             required
-                            defaultValue={ userRecipe !== undefined ? userRecipe.recipeName : '' }
+                            defaultValue={userRecipe !== undefined ? userRecipe.recipeName : ''}
                         />
                     </div>
                     <div className="my-6">
@@ -127,7 +127,7 @@ export default function UserRecipeEdit() {
                             className="block w-1/2 min-w-[400px] max-w-2xl p-2.5 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-sky-500 focus:border-sky-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-sky-500 dark:focus:border-sky-500"
                             {...register('recipeInstructions')}
                             required
-                            defaultValue={ userRecipe !== undefined ? userRecipe.recipeInstructions : ''}
+                            defaultValue={userRecipe !== undefined ? userRecipe.recipeInstructions : ''}
                             />
                     </div>
                     <div className="my-6">
